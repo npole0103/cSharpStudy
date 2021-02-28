cc581dd46ecb12d3576fbf9f225386dcbc5a81f6
npole0103 <npole0103@naver.com>
2021-03-01 02:11:31 +0900

    Hello World & stdIO

---
+++ cSharpPracting/StdIO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StdIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strName;
+            Console.Write("당신의 이름은 ? ");
+            strName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}!", strName);
+        }
+    }
+}
