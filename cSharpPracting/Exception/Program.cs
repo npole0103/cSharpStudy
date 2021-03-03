aedab8308671ccd77c58dddab7ff1819b3e13677
npole0103 <npole0103@naver.com>
2021-03-03 20:04:01 +0900

    foreach & Exception

---
+++ cSharpPracting/Exception/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str = null;
+            try
+            {
+                Console.WriteLine(str.ToString());
+                Console.WriteLine("Program Terminated");
+            }
+            catch(NullReferenceException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+
+            int[] arr1 = { 1, 11, 22, 33 };
+            int[] arr2 = { 0, 1, 2 };
+            for(int i = 0; i < arr1.Length; i++)
+                try
+                {
+                    Console.WriteLine(arr1[i] + "/" + arr2[i] + "=" + arr1[i] / arr2[i]);
+                }
+                catch(DivideByZeroException e)
+                {
+                    Console.WriteLine("Cant divide");
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+        }
+    }
+}
---
+++ cSharpPracting/IterationStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections; //ArrayList
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        public static void PrintValue(ArrayList myList)
+        {
+            foreach (string text in myList)
+                Console.Write("\t{0}", text);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            ArrayList myAl = new ArrayList();
+            myAl.Add("Hello");
+            myAl.Add("World");
+            myAl.Add("!");
+            Console.Write("Values:");
+            PrintValue(myAl);
+        }
+    }
+}
